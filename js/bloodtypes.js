44ff6b14ed91f4581189679cff133b1b1b8d0e43
harvardadrianaa <adrianarossettisugih@g.harvard.edu>
2014-11-06 15:24:30 +0900

    forms hover effect fixed

--- js/bloodtypes.js
+++ js/bloodtypes.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 
 	$('table th').css({
-		backgroundColor: "#A0A0A0 "
+		backgroundColor: "#000099",
+		color: "#fff"
 	});
 
 	$('table tr:even').css({
-		backgroundColor: "#CCFFFF"
+		backgroundColor: "#C0C0C0"
 	});
 
+	// changing color on mouse hover
+	$('table tr').hover(function(){ $(this).css({backgroundColor: "#3300FF"}) },
+			   function(){ 
 
-
-
-
+				$('table tr:even').css({ backgroundColor: "#C0C0C0" }) ;
+			   	$('table tr:odd').css({ backgroundColor: "#fff" })
+			   	}
+			   );
 });
---
+++ js/inde.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	console.log('Ready!');
+
+    	$('article a').tooltip();
+	$('header').fadeOut(3000).fadeIn('slow');
+
+
+
+
+});
