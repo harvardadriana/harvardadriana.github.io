fee0a1a47c18707be8135e91b8e3ce84f0d0e519
harvardadrianaa <adrianarossettisugih@g.harvard.edu>
2014-11-07 14:26:20 +0900

    All bugs fixed

--- js/bloodtypes.js
+++ js/bloodtypes.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
 	$('table th').css({
-		backgroundColor: "#000099",
-		color: "#fff"
+		backgroundColor: "olive",
+		color: "#000000"
 	});
 
 	$('table tr:even').css({
 		backgroundColor: "#C0C0C0"
 	});
 
+	$('table tr:odd').css({
+		backgroundColor: "#fff"
+	});
+
 	// changing color on mouse hover
-	$('table tr').hover(function(){ $(this).css({backgroundColor: "#3300FF"}) },
+	$('table tr').hover(function(){ $(this).css({backgroundColor: "#EEE8AA" }) },
 			   function(){ 
 
 				$('table tr:even').css({ backgroundColor: "#C0C0C0" }) ;
